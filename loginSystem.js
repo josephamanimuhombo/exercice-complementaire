fbf22b04efaa322f8c7c197857c22088006dc46b
Joseph Amani Muhombo <josephamanimuhombo@gmail.com>
2025-12-03 11:34:24 +0100

    Résolution deuxieme exercice

--- loginSystem.js
+++ loginSystem.js
@@ -1,3 +1,7 @@
 let motDePasse = prompt("Entrez le mot de passe :");
 const SECRET = "12345";
-if ("mot de passe=== SECRET")
+if ("mot de passe=== SECRET"){
+    console.log("Acces accordé");
+} else{
+    console.log("Mot de passe incorect");
+}
